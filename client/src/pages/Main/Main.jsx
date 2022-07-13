22aac27f8ce3a56e45114f8d6d058fdd8d35172c
Artemka0-7 <pavlovskiy.07@inbox.ru>
2022-07-13 16:04:04 +0300

    build 3

--- client/src/pages/Main/Main.jsx
+++ client/src/pages/Main/Main.jsx
@@ -6,7 +6,7 @@ import Top from '../../components/screens/Home/Top'
 
 const Main = () => {
   window.scrollTo({ top: 0, behavior: 'auto' })
-  document.title = "Сенатмед: продажа медицинского оборудования"
+  document.title = "продажа медицинского оборудования - Сенатмед"
   return (
     <>
       <Top />
